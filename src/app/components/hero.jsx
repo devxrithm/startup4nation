e9fc1310c2e3748526b46e3aa5a1ce19af235bf0
Ujjwal Katiyar <ukcode07@gmail.com>
2024-12-25 14:50:46 +0530

    add files

---
+++ src/app/components/hero.jsx
@@ -0,0 +1,67 @@
+import { cn } from "@/lib/utils";
+import AnimatedGridPattern from "@/components/ui/animated-grid-pattern";
+import { ArrowRightIcon } from "@radix-ui/react-icons";
+import AnimatedShinyText from "@/components/ui/animated-shiny-text";
+import { Cover } from "@/components/ui/cover";
+import Image from "next/image";
+
+export default async function Hero() {
+    return (
+        <>
+            <div className="relative min-h-[25rem] pt-52">
+
+                <AnimatedGridPattern
+                    numSquares={10}
+                    maxOpacity={0.2}
+                    duration={3}
+                    repeatDelay={1}
+                    className={cn(
+                        "[mask-image:radial-gradient(700px_circle_at_center,white,transparent)]",
+                        "inset-x-0 inset-y-[-30%] h-[200%] skew-y-20",
+                    )}
+                />
+
+                <div className="flex flex-col items-center justify-center">
+                    <div className="z-20 flex min-h-14 items-center justify-center">
+                        <div
+                            className={cn(
+                                "group rounded-full border border-black/5 bg-neutral-100 text-base text-white transition-all ease-in hover:cursor-pointer hover:bg-neutral-200 dark:border-white/5 dark:bg-neutral-900 dark:hover:bg-neutral-800",
+                            )}
+                        >
+                            <AnimatedShinyText className="inline-flex items-center justify-center px-4 py-1 transition ease-out hover:text-neutral-600 hover:duration-300 hover:dark:text-neutral-400 font-semibold text-sm">
+                                <span>✨ Welcome Back To</span>
+                                <ArrowRightIcon className="ml-1 size-3 transition-transform duration-300 ease-in-out group-hover:translate-x-0.5" />
+                            </AnimatedShinyText>
+                        </div>
+                    </div>
+
+                    <div className="flex justify-center items-center gap-3 -mt-9">
+                        <Image
+                            src={"/logo.png"}
+                            width={130}
+                            height={130}
+                            alt="logo"
+                        />
+                        <p className="z-10 whitespace-pre-wrap text-center text-6xl tracking-tighter text-gray-800 dark:text-white font-extrabold">
+                            STARTUP<span className="text-7xl italic text-transparent text-stroke-black">4</span>NATION
+                        </p>
+                    </div>
+
+                    <div className="-mt-10">
+                        <h1 className="text-4xl md:text-4xl lg:text-2xl font-semibold max-w-7xl mx-auto text-center relative z-20 py-6 bg-clip-text text-transparent bg-gradient-to-b from-neutral-800 via-neutral-700 to-neutral-700 dark:from-neutral-800 dark:via-white dark:to-white">
+                            WHERE CRAZY BECOME <Cover>CREATIVE</Cover>
+                        </h1>
+                    </div>
+
+
+                </div>
+            </div>
+            {/* <div className=" flex justify-center items-center gap-3 mt-10">
+                <button className="px-8 py-2  bg-black text-white text-sm rounded-md font-semibold hover:bg-black/[0.8] hover:shadow-lg flex items-center justify-center">
+                    Favourite
+                </button>
+            </div> */}
+        </>
+    );
+}
+
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,17 +1,60 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
-};
+const {
+	default: flattenColorPalette,
+  } = require("tailwindcss/lib/util/flattenColorPalette");
+  
+  /** @type {import('tailwindcss').Config} */
+  module.exports = {
+	content: [
+	  "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+	  "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+	  "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  
+	  // Or if using `src` directory:
+	  "./src/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	darkMode: "class",
+	theme: {
+	  extend: {
+		
+		animation: {
+			shimmer: "shimmer 2s linear infinite",
+		  },
+		  keyframes: {
+			shimmer: {
+			  from: {
+				backgroundPosition: "0 0",
+			  },
+			  to: {
+				backgroundPosition: "-200% 0",
+			  },
+			},
+		  },
+	  },
+	},
+	plugins: [addVariablesForColors,
+		function ({ addUtilities }) {
+			addUtilities({
+			  '.text-stroke': {
+				'-webkit-text-stroke': '1px',
+			  },
+			  '.text-stroke-black': {
+				'-webkit-text-stroke': '1px black',
+			  },
+			});
+		  },
+	],
+  };
+  
+  // This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
+  function addVariablesForColors({
+	addBase,
+	theme
+  }) {
+	let allColors = flattenColorPalette(theme("colors"));
+	let newVars = Object.fromEntries(Object.entries(allColors).map(([key, val]) => [`--${key}`, val]));
+  
+	addBase({
+	  ":root": newVars,
+	});
+  }
+  
